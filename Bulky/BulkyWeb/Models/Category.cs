d4440d14e27b5d60b294bb5fa5e5841dba996f8c
braim23 <94292623+braim23@users.noreply.github.com>
2023-07-03 09:56:56 +0300

    initial3

--- Bulky/BulkyWeb/Models/Category.cs
+++ Bulky/BulkyWeb/Models/Category.cs
@@ -4,7 +4,9 @@ namespace BulkyWeb.Models;
 
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
 
     public int DisplayOrder { get; set; }
--- Bulky/BulkyWeb/Program.cs
+++ Bulky/BulkyWeb/Program.cs
@@ -1,7 +1,12 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(opt=>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
