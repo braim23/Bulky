0795dd31a5ab92477582199087b663e46d00ffb9
Hamza Albreem <94292623+braim23@users.noreply.github.com>
2023-07-12 18:38:26 +0300

    Added Category Create

--- Bulky/BulkyWeb/Models/Category.cs
+++ Bulky/BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models;
 
@@ -7,7 +8,8 @@ public class Category
     [Key]
     public int Id { get; set; }
     [Required]
+    [DisplayName("Category Name")]
     public string Name { get; set; }
-
+    [DisplayName("Display Order")]
     public int DisplayOrder { get; set; }
 }
